1bd29373264ad2c8be30d32f45825502ebcec3b9
charlie reyes <charliereyes1228@gmail.com>
2022-08-14 16:44:11 -0700

    arrow key function added but continues to move after input

--- index.js
+++ index.js
@@ -23,6 +23,27 @@ if (direction=== 'south') {
 character.style.left = x + 'px'
 character.style.bottom = y+ 'px'
 }
+setInterval(moveCharacter, 1)                                    
+
+document.addEventListener('keydown', function(e){  
+    if (e.repeat) return;              
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+})
+
+
+
+
 move(character).to(100, 250)
 
 
